ec0106c12194804683ed8cdbf3f3d17032e7e231
Damian Perera <damian@perera.io>
2018-03-20 22:04:42 +0530

    Completed converter model started work on first view controller

--- iconverter/iconverter/ConverterModel.swift
+++ iconverter/iconverter/ConverterModel.swift
@@ -41,7 +41,7 @@ class ConverterModel {
     ]
     
     func getName(unit of: Unit) -> String {
-        var result = ""
+        var result:String = unitNames[.kg]!
         for (val, name) in unitNames {
             if val == of {
                 result = name
@@ -52,14 +52,15 @@ class ConverterModel {
     }
     
     func getUnit(name of: String) -> Unit {
-        var result = Unit.kg
-        for (val, name) in unitNames {
-            if name == of {
-                result = val
-                break
-            }
-        }
-        return result
+        return (unitNames as NSDictionary).allKeys(for: of)[0] as! Unit
+//        var result:Unit = .kg
+//        for (val, name) in unitNames {
+//            if name == of {
+//                result = val
+//                break
+//            }
+//        }
+//        return result
     }
     
     func convert(unit from: Unit, value val: Double) -> Dictionary<Unit, Double> {
@@ -102,64 +103,64 @@ class ConverterModel {
             values[.C] = val - 273.15
             values[.F] = val * 9/5 - 459.67
         case .cm:
-            values[.m] = val
-            values[.ih] = val
-            values[.mm] = val
-            values[.yd] = val
+            values[.m] = val * 0.01
+            values[.ih] = val * 0.393701
+            values[.mm] = val * 10
+            values[.yd] = val * 0.0109361
         case .m:
-            values[.cm] = val
-            values[.ih] = val
-            values[.mm] = val
-            values[.yd] = val
+            values[.cm] = val * 100
+            values[.ih] = val * 39.3701
+            values[.mm] = val * 1000
+            values[.yd] = val * 1.09361
         case .ih:
-            values[.cm] = val
-            values[.m] = val
-            values[.mm] = val
-            values[.yd] = val
+            values[.cm] = val * 2.54
+            values[.m] = val * 0.0254
+            values[.mm] = val * 25.4
+            values[.yd] = val * 0.0277778
         case .mm:
-            values[.cm] = val
-            values[.m] = val
-            values[.ih] = val
-            values[.yd] = val
+            values[.cm] = val * 0.1
+            values[.m] = val * 0.001
+            values[.ih] = val * 0.0393701
+            values[.yd] = val * 0.00109361
         case .yd:
-            values[.cm] = val
-            values[.m] = val
-            values[.ih] = val
-            values[.mm] = val
+            values[.cm] = val * 91.44
+            values[.m] = val * 0.9144
+            values[.ih] = val * 36
+            values[.mm] = val * 914.4
         case .gal:
-            values[.l] = val
-            values[.pt] = val
-            values[.fl_oz] = val
+            values[.l] = val * 3.78541
+            values[.pt] = val * 8
+            values[.fl_oz] = val * 128
         case .l:
-            values[.gal] = val
-            values[.pt] = val
-            values[.fl_oz] = val
+            values[.gal] = val * 0.264172
+            values[.pt] = val * 2.11338
+            values[.fl_oz] = val * 33.814
         case .pt:
-            values[.gal] = val
-            values[.l] = val
-            values[.fl_oz] = val
+            values[.gal] = val * 0.125
+            values[.l] = val * 0.473176
+            values[.fl_oz] = val * 16
         case .fl_oz:
-            values[.gal] = val
-            values[.l] = val
-            values[.pt] = val
+            values[.gal] = val * 0.0078125
+            values[.l] = val * 0.0295735
+            values[.pt] = val * 0.0625
         case .m3:
-            values[.cm3] = val
-            values[.l3] = val
+            values[.cm3] = val * 1000000
+            values[.l3] = val * 1000
         case .cm3:
-            values[.m3] = val
-            values[.l3] = val
+            values[.m3] = val * 1e-6
+            values[.l3] = val * 0.001
         case .l3:
-            values[.m3] = val
-            values[.cm3] = val
+            values[.m3] = val * 0.001
+            values[.cm3] = val * 1000
         case .m_s:
-            values[.km_h] = val
-            values[.ml_h] = val
+            values[.km_h] = val * 3.6
+            values[.ml_h] = val * 2.23694
         case .km_h:
-            values[.m_s] = val
-            values[.ml_h] = val
+            values[.m_s] = val * 0.277778
+            values[.ml_h] = val * 0.621371
         case .ml_h:
-            values[.m_s] = val
-            values[.km_h] = val
+            values[.m_s] = val * 0.44704
+            values[.km_h] = val * 1.60934
         }
 
         return values
--- iconverter/iconverter/FirstViewController.swift
+++ iconverter/iconverter/FirstViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var testTextField: UITextField!
+    var keyBoardHeight:CGFloat = 0
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)),
+                                               name: .UIKeyboardWillShow, object: nil)
+        testTextField.becomeFirstResponder()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)),
+                                               name: .UIKeyboardWillShow, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        //this moves the tab bar above the keyboard for all devices
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as?
+            NSValue)?.cgRectValue {
+            self.keyBoardHeight = keyboardSize.origin.y - keyboardSize.height -
+                (self.tabBarController?.tabBar.frame.height)!
+        }
+//        tabBarMoved = true
+        var tabBarFrame: CGRect = (self.tabBarController?.tabBar.frame)!
+        tabBarFrame.origin.y = self.keyBoardHeight
+        UIView.animate(withDuration: 0.25, animations: {() -> Void in
+            self.tabBarController?.tabBar.frame = tabBarFrame
+        })
     }
 
     override func didReceiveMemoryWarning() {

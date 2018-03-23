63dab2484c114bd4227690a5b2919e05ba360463
Damian Perera <damian@perera.io>
2018-03-23 23:39:34 +0530

    Added view connections to distance controller

--- iconverter/iconverter/Controllers/DistanceController.swift
+++ iconverter/iconverter/Controllers/DistanceController.swift
@@ -6,4 +6,50 @@
 //  Copyright © 2018 Damian Perera. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class DistanceController: UIViewController {
+    
+    @IBOutlet weak var txtCentimetre: UITextField!
+    @IBOutlet weak var txtMillimetre: UITextField!
+    @IBOutlet weak var txtYard: NSLayoutConstraint!
+    @IBOutlet weak var txtInch: UITextField!
+    @IBOutlet weak var txtMetre: UITextField!
+    var keyBoardHeight:CGFloat = 0
+    var isKeyboardActive = false
+    var tabBarOGHeight:CGFloat = 0
+    var didSegue = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    /**
+        Logical Components
+    **/
+    
+    /**
+        Action Components
+    **/
+    
+    @IBAction func txtCentimetreOnEdit(_ sender: UITextField) {
+    }
+    
+    @IBAction func txtMetreOnEdit(_ sender: UITextField) {
+    }
+    
+    @IBAction func txtInchOnEdit(_ sender: UITextField) {
+    }
+    
+    @IBAction func txtMillimetreOnEdit(_ sender: UITextField) {
+    }
+    
+    @IBAction func txtYardOnEdit(_ sender: UITextField) {
+    }
+    
+    
+    /**
+        UI Components
+    **/
+    
+}

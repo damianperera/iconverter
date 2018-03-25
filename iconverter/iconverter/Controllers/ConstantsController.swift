b15173848d8eae8766cda4cf598bab58ea6a6950
Damian Perera <damian@perera.io>
2018-03-25 18:59:37 +0530

    Constants UI and coding done

--- iconverter/iconverter/Controllers/ConstantsController.swift
+++ iconverter/iconverter/Controllers/ConstantsController.swift
@@ -11,10 +11,61 @@ import UIKit
 
 class ConstantsController: UIViewController {
     
+    @IBOutlet weak var lblMagneticPermeability: UILabel!
+    @IBOutlet weak var lblElectricPermittivity: UILabel!
+    @IBOutlet weak var lblNeutronMass: UILabel!
+    @IBOutlet weak var lblProtonMass: UILabel!
+    @IBOutlet weak var lblElectronMass: UILabel!
+    
     var segueFromController:String!
+    let font:UIFont? = UIFont(name: "Helvetica", size:20)
+    let fontSuper:UIFont? = UIFont(name: "Helvetica", size:12)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadElectronMass()
+        loadProtonMass()
+        loadNeutronMass()
+        loadElectricPermittivity()
+        loadMagneticPermeability()
+    }
+    
+    func loadElectronMass() {
+        let attString:NSMutableAttributedString = NSMutableAttributedString(string: "me = 9.109xxxxx x 10−31 (kg)", attributes: [.font:font!])
+        attString.setAttributes([.font:fontSuper!,.baselineOffset:10], range: NSRange(location:20,length:3))
+        attString.setAttributes([.font:fontSuper!,.baselineOffset:-5], range: NSRange(location:1,length:2))
+        lblElectronMass.attributedText = attString
+    }
+    
+    func loadProtonMass() {
+        let attString:NSMutableAttributedString = NSMutableAttributedString(string: "mp = 1.672xxxxx x 10−27 (kg)", attributes: [.font:font!])
+        attString.setAttributes([.font:fontSuper!,.baselineOffset:10], range: NSRange(location:20,length:3))
+        attString.setAttributes([.font:fontSuper!,.baselineOffset:-5], range: NSRange(location:1,length:2))
+        lblProtonMass.attributedText = attString
+    }
+    
+    func loadNeutronMass() {
+        let attString:NSMutableAttributedString = NSMutableAttributedString(string: "mn = 1.674xxxxx x 10−27 (kg)", attributes: [.font:font!])
+        attString.setAttributes([.font:fontSuper!,.baselineOffset:10], range: NSRange(location:20,length:3))
+        attString.setAttributes([.font:fontSuper!,.baselineOffset:-5], range: NSRange(location:1,length:2))
+        lblNeutronMass.attributedText = attString
+    }
+    
+    func loadElectricPermittivity() {
+        let attString:NSMutableAttributedString = NSMutableAttributedString(string: "ε0 = 8.854xxxxx x 10−12 C2/Nm2", attributes: [.font:font!])
+        attString.setAttributes([.font:fontSuper!,.baselineOffset:10], range: NSRange(location:29,length:1))
+        attString.setAttributes([.font:fontSuper!,.baselineOffset:10], range: NSRange(location:25,length:1))
+        attString.setAttributes([.font:fontSuper!,.baselineOffset:10], range: NSRange(location:20,length:3))
+
+        attString.setAttributes([.font:fontSuper!,.baselineOffset:-5], range: NSRange(location:1,length:2))
+        lblElectricPermittivity.attributedText = attString
+    }
+    
+    func loadMagneticPermeability() {
+        let attString:NSMutableAttributedString = NSMutableAttributedString(string: "μ0 = 4π x 10−7 Tm/A", attributes: [.font:font!])
+        attString.setAttributes([.font:fontSuper!,.baselineOffset:10], range: NSRange(location:12,length:2))
+        attString.setAttributes([.font:fontSuper!,.baselineOffset:-5], range: NSRange(location:1,length:2))
+        lblMagneticPermeability.attributedText = attString
     }
 
     @IBAction func cancelSegue(_ sender: UIBarButtonItem) {

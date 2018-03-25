c295fe7d994f649ede5719cc6bb9395603051c8a
Damian Perera <damian@perera.io>
2018-03-25 13:35:56 +0530

    Linked UI and functions

--- iconverter/iconverter/Controllers/LiquidsController.swift
+++ iconverter/iconverter/Controllers/LiquidsController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class LiquidsController: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var txtGallon: UITextField!
+    @IBOutlet weak var txtLitre: UITextField!
+    @IBOutlet weak var txtPint: UITextField!
+    @IBOutlet weak var txtFluidOunce: UITextField!
+    
     var keyBoardHeight:CGFloat = 0
     var isKeyboardActive = false
     var tabBarOGHeight:CGFloat = 0
@@ -17,7 +22,24 @@ class LiquidsController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        txtGallon.delegate = self
+        txtLitre.delegate = self
+        txtPint.delegate = self
+        txtFluidOunce.delegate = self
     }
+    
+    @IBAction func txtGallonOnEdit(_ sender: UITextField) {
+    }
+    
+    @IBAction func txtLitreOnEdit(_ sender: UITextField) {
+    }
+    
+    @IBAction func txtPintOnEdit(_ sender: UITextField) {
+    }
+    
+    @IBAction func txtFluidOunceOnEdit(_ sender: UITextField) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
